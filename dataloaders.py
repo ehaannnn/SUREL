d7ce0766514530bd28c79e872b8b5f065e7fbdad
hangyul.yi <hangyul.yi@samsung.com>
2022-05-16 18:06:44 +0900

    fix minor issue

--- dataloaders.py
+++ dataloaders.py
@@ -33,29 +33,29 @@ class Yelp():
 
             #Importing Yelp business data
             with open(os.path.join(dir, 'yelp_academic_dataset_business.json'), encoding='utf-8') as json_file:
-            data = [json.loads(line) for line in json_file]    
-            df_yelp_business = pd.DataFrame(data)
+                data = [json.loads(line) for line in json_file]    
+                df_yelp_business = pd.DataFrame(data)
             
             #Importing Yelp review data
             with open(os.path.join(dir, 'yelp_academic_dataset_review.json'), encoding='utf-8') as json_file:
-            data = [json.loads(line) for line in json_file]    
-            df_yelp_review = pd.DataFrame(data)
+                data = [json.loads(line) for line in json_file]    
+                df_yelp_review = pd.DataFrame(data)
 
         
             #Importing Yelp user data
             with open(os.path.join(dir, 'yelp_academic_dataset_user.json'), encoding='utf-8') as json_file:
-            data = [json.loads(line) for line in json_file]    
-            df_yelp_user = pd.DataFrame(data)
+                data = [json.loads(line) for line in json_file]    
+                df_yelp_user = pd.DataFrame(data)
             
             #Importing Yelp checkin data
             with open(os.path.join(dir, 'yelp_academic_dataset_checkin.json'), encoding='utf-8') as json_file:
-            data = [json.loads(line) for line in json_file]    
-            df_yelp_checkin = pd.DataFrame(data)
+                data = [json.loads(line) for line in json_file]    
+                df_yelp_checkin = pd.DataFrame(data)
                 
             #Importing Yelp tip data
             with open(os.path.join(dir, 'yelp_academic_dataset_tip.json'), encoding='utf-8') as json_file:
-            data = [json.loads(line) for line in json_file]    
-            df_yelp_tip = pd.DataFrame(data) 
+                data = [json.loads(line) for line in json_file]    
+                df_yelp_tip = pd.DataFrame(data) 
                 
             return df_yelp_business, df_yelp_user, df_yelp_review, df_yelp_tip, df_yelp_checkin
 
